5d0e9615fd9861b1e6c7fbf7bea53837e4658741
Jackson Leb <jacksonleb@Jacksons-MacBook-Pro.local>
2022-02-17 16:43:59 -0500

    Adding groupId retrieval necessary to establishing a bot in a chat

--- bot_setup.py
+++ bot_setup.py
@@ -2,6 +2,13 @@ import json
 import requests
 from access_token import access_token
 
+#parsing through General json info to get groupID
+def findGroupID(group_name, json_info):
+    for group in json_info["response"]:
+        if (group["name"] == group_name):
+            id = group["group_id"]
+            return id
+
 #url for retrieving group ID's
 url = 'https://api.groupme.com/v3/groups?token='
 url += access_token
@@ -10,6 +17,19 @@ url += access_token
 r = requests.get(url)
 json_data = r.json()
 
+#groupID retrieval
+group_name = "Test Group"
+group_id = findGroupID(group_name, json_data)
+print(group_id)
+
+
 #ouptut json
 with open('data.json', 'w', encoding='utf-8') as outfile:
     json.dump(json_data, outfile, ensure_ascii=False, indent=4)
+
+
+
+
+
+
+
